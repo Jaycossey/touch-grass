943cc140615004e25f1531fe91a9f73f4fabc5d7
jaycossey <jaycossey@live.com>
2023-09-30 21:53:07 +0100

    added more JS functions and animation on hover for first grass blade

--- scripts.js
+++ scripts.js
@@ -5,7 +5,8 @@ console.log(cloudContainer);
 console.log(grassContainer);
 
 const MAX_CLOUDS = 3;
-const MAX_GRASS = 100;
+const MAX_GRASS = 50;
+const SCREEN_WIDTH = window.innerWidth;
 
 let grassCounter = 0;
 let cloudCounter = 0;
@@ -34,18 +35,56 @@ let cloudCounter = 0;
  * 
  */
 
+// GRASS FUNCTIONS ---------------------------------------------------------------------------------------
+
 // grass array
-const grassArray = [MAX_GRASS];
+const grassArray = [];
 
 // grass object constructor
 function Grass(xPosition, color, height) {
+    // constructor bindings
     this.xPosition = xPosition;
     this.color = color;
-    this.height = height;
-
-    this.width = "10px";
+    this.height = height + "px";
+    this.width = "12px";
     this.class = "grass";
+    
+    // Grass div generation and basic style + position
+    let bladeGrass = document.createElement('div');
+    bladeGrass.className = "bladeOfGrass";
+    bladeGrass.style.borderLeft = "2px solid green";
+    bladeGrass.style.borderTop = "15px solid green";
+    bladeGrass.style.width = this.width;
+    bladeGrass.style.height = this.height;
+    bladeGrass.style.left = this.xPosition + "px";
+    bladeGrass.style.marginBottom = "10px";
+
+    // add blade to screen
+    grassContainer.appendChild(bladeGrass);
+    // console.log(bladeGrass);
+    // console.log(grassArray);
 }
+
+// When this function is called, generate a new grass object
+function grassGenerator(currentGrassCount) {
+    let randomGrassHeight = Math.floor(Math.random() * 81 + 40);
+    let randomXPosition = Math.floor(Math.random() * SCREEN_WIDTH);
+
+    // if the current grass count is less than the max grass const
+    if (currentGrassCount <= MAX_GRASS) {
+        // push new grass into array and increment the counter
+        grassArray.push(new Grass(randomXPosition, "green", randomGrassHeight));
+        grassCounter++;
+        console.log(grassArray);
+    } else if (currentGrassCount >= MAX_GRASS) {
+        // else trigger wildfire error, eventually triggering the wildfire function to reset the page
+        console.log("Wildfire!");
+    }
+
+    return;
+}
+
+// CLOUD FUNCTIONS -------------------------------------------------------------------------------------
 
 // clouds array 
 const cloudArray = [MAX_CLOUDS];
@@ -59,24 +98,29 @@ function Cloud(width, height, bubbleCount) {
     this.class = "cloud";
 }
 
+// SUN FUNCTIONS --------------------------------------------------------------------------------------
+
 // generate sun function, issue with radial gradient, check syntax errors and go from there.
 function generateSun() {
-    console.log("sun!");
-    // let sunGradient = "radial-gradient(#F87DOA 0%, #F2F80A 90%, transparent 100%)";
+    // console.log("sun!");
 
     let sun = document.createElement('div');
     sun.id = "sun";
     sun.style.width = "150px";
     sun.style.height = "150px";
-    sun.style.border = "6px solid yellow";
-    sun.style.borderRadius = "75px";
+    // sun.style.border = "6px solid yellow";
+    sun.style.borderRadius = "80px";
     sun.style.position = "fixed";
     sun.style.top = 0;
     sun.style.right = 0;
-    // sun.style.backgroundImage = sunGradient;
+    sun.style.backgroundImage = "radial-gradient(orange 40%, transparent 80%)";
+    sun.style.animation = "pulse";
+    sun.style.animationDuration = "1s";
+    sun.style.animationIterationCount = "infinite";
 
     cloudContainer.appendChild(sun);
-    console.log(sun);
+    // console.log(sun);
 }
 
 document.onload = generateSun();
+document.onload = grassGenerator(grassCounter);
